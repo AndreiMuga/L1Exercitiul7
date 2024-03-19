aa40a2f648c0e1904bd254e3c2a026a4c0ec37ac
Andrei <Andrei@DESKTOP-K91SUSC>
2024-03-19 21:54:44 +0200

    Add project files.

---
+++ Tema1Exercitiul7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace Lab1Ex7
+/*
+Ex 7
+Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+daca x este divizibil cu y.
+ In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar,
+“indivizibil”.
+ */
+{
+    public class Program 
+    { public static void Main(string[] args) 
+        { 
+            double primulNumar;
+            double alDoileaNumar;
+            double divizibil;
+            Console.WriteLine("Introduceti primul numarul: ");
+            primulNumar = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea numar: ");
+            alDoileaNumar = Convert.ToDouble(Console.ReadLine());
+            divizibil = primulNumar % alDoileaNumar;
+            if (divizibil == 0)
+            { 
+                Console.WriteLine("Numerele sunt dibizibile");
+            } 
+            else
+            {
+                Console.WriteLine("Numerele sunt indivizibile");
+            }
+        }
+    }
+}
